591a2c72adca10e0bb911b5637b7688fa489fcca
haejunlee <haejun0410@gmail.com>
2022-10-08 21:34:51 +0900

    sum_opt

---
+++ sum_opt.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<limits.h>
+
+#define MAX_NUM 100000000
+//#define MAX_NUM 1000000000
+//#define MAX_NUM INT_MAX
+//#define MAX_NUM 100000
+
+struct vec {
+	int len;
+	int *data;
+};
+typedef struct vec vec_t;
+
+int get_vec_element(vec_t* v, int idx, int *val)
+{
+	assert(v);
+
+	if(idx >= v->len)
+		return 0;
+
+	*val = v->data[idx];
+	return 1;
+}
+
+int* get_vec_start(vec_t* v){
+	return v->data;
+}
+
+int vec_length(vec_t* v)
+{
+	assert(v);
+	return v->len;
+}
+
+void combine(vec_t* v, int *dest)
+{
+	assert(dest);
+	*dest = 0;
+	int temp = 0;
+	int *d = get_vec_start(v);
+	long v_len = vec_length(v);
+	for(int i = 0; i < v_len; i++){
+		temp = temp + d[i]; 
+	}
+	*dest = temp;
+}
+
+void init(vec_t* v)
+{
+	assert(v);
+	v->len = MAX_NUM;
+	v->data = (int*) malloc(sizeof(int) * MAX_NUM);
+
+	// init 
+	for(int i = 0; i < MAX_NUM; i++)
+		v->data[i] = i;
+}
+
+int main()
+{
+	printf("This is the naive version .. \n");
+
+	vec_t info; 
+	int result; 
+	// init 
+	init(&info);
+
+	// combine 
+	combine(&info, &result);
+	printf("combined val = %d\n", result);
+
+	return 0;
+}
